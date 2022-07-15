9c86ff72a945254e591967135b0bba1b935e59d8
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-07-15 15:47:18 -0300

    Modificado arquivo variaveis.py

--- automacao/variaveis.py
+++ automacao/variaveis.py
@@ -1,4 +1,19 @@
+#Declarando as variaveis
+personagem_nome='Alexandre'
+personagem_idade='43'
+personagem_esporte = 'boxe'
+persongaem_jogo = 'GTA 5'
+
+#Utilizando o print sem variaveis
 print('Meu nome e Marcos, ')
 print('Eu tenho 19 anos de idade.')
 print('Meu esporte favorito e o futebol, ')
 print('eu gosto de jogar fortnit nas minhas horas vagas.')
+#Quebra de linha em python
+print('\n')
+
+#Utilizando o print com variaveis
+print('Meu nome e ', personagem_nome, ' !!! ')
+print('Eu tenho ', personagem_idade , 'anos de idade.')
+print('Meu esporte favorito e o ',personagem_esporte)
+print('eu gosto de jogar ',persongaem_jogo , 'nas minhas horas vagas.')
