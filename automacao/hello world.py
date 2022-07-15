0aee0585a678ee84d1bb1b5ed5af4ffa8b1c7183
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-07-15 14:45:00 -0300

    Inicio do projeto

---
+++ automacao/hello world.py
@@ -0,0 +1,7 @@
+#Primeiro escript em python
+#Função print escreve um texto na tela
+#Obs: primeiro exemplo usando aspas simples '
+print('Hello World !!!')
+
+#Obs2: segundo exemplo usando aspas duplas "
+print("Hello world2 !!!")
