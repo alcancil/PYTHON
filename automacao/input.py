78fb1e682f2886e4bbcf658284047d77530713e4
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-07-25 09:38:47 -0300

    Adicionado o arquivo input.py

---
+++ automacao/input.py
@@ -0,0 +1,5 @@
+#Aprendendo como funciona o input
+nome = input('Digite o seu nome : ')
+idade = input('Digite a sua idade: ')
+
+print('Olá ' + nome + '.' + ' Hoje você tem ' + idade + ' anos de idade.')
