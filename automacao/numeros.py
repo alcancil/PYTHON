a90ee754ce18857c0681fb510f72e9185486547a
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-07-25 09:24:20 -0300

    Adicionado arquivo numeros.py

---
+++ automacao/numeros.py
@@ -0,0 +1,27 @@
+numero1 = 5
+numero2 = 10
+#Se escrevermos o número 6 sem aspas na função print, ele irá retornar o número 6 e não uma string
+print(6)
+#Imprimindo em tela númeors negativos
+print(-8)
+#Imprimindo números quebrados
+print(98.290)
+print(-101.7870)
+#Dentro do print é possível realizar cálculos matemáticos
+print(4 + 2)
+print(4 + 2.345)
+print(2 * 2 + 5)
+print(2 * (2 + 5))
+#Cálculos com variáveis
+print(numero1+numero2)
+print(numero1-numero2)
+#Números com strings --- str(string) --- nesse caso string é um inteiro e dessa forma converti um inteiro para uma string
+print(str(numero1) + ' é o meu número da sorte !!!')
+#Função min retorna o menor número passado nos parâmetros
+print(min(2, 8, 1))
+#Função max retorna o menor número passado nos parâmetros
+print(max(2, 8, 1))
+#Função potencia
+print(pow(3,2))
+#Função arrendodar
+print(round(12.75))
