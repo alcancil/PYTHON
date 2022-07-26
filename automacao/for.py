f4bab574394e8dc421f81f030ee431062ad13607
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-07-25 23:41:29 -0300

    Adcionando o arquivo loop.py

---
+++ automacao/for.py
@@ -0,0 +1,17 @@
+#Aprendendo sobre loops com o FOR
+amigos = ['Paulo', "Pedro", 'Roberto', 'Tiao', 'Tonho']
+
+for letra in 'google' :
+    print(letra)
+
+for amigos in amigos :
+    print(amigos)
+
+#imprimir numeros em um range de 10
+for numeros in range(10) :
+    print(numeros)
+
+print()
+
+for numeros in range(2,20) :
+    print(numeros)
