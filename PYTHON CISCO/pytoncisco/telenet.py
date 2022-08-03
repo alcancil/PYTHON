39a8abd8dd40dc316b142d88a554c0e73cf6d5f0
Alexandre Lavorenti Cancilieri <alcancil@gmail.com>
2022-08-03 11:21:23 -0300

    Realizado ajustes no arquivo telnet.py

--- PYTHON CISCO/pytoncisco/telenet.py
+++ PYTHON CISCO/pytoncisco/telenet.py
@@ -14,9 +14,13 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
+tn.write(b"terminal monitor\n")
+tn.write(b"debug telnet\n")
 tn.write(b"conf t\n")
 tn.write(b"int loopback 0\n")
 tn.write(b"ip address 1.1.1.1 255.255.255.0\n")
+tn.write(b"shut\n")
+tn.write(b"no shut\n")
 tn.write(b"end\n")
 tn.write(b"exit\n")
 
